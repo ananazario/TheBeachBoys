01cefffc5669de74538290e8eb4012895e8dde49
Ana Carolina <101834138+ananazario@users.noreply.github.com>
2022-08-19 21:29:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Variaveis
+var corpo = window.document.body
+var botao = document.getElementsByClassName('botao')[0]
+var span = document.getElementById('titulo-span__cabecalho')
+
+corpo.style.background = 'rgb(59, 59, 59)'
+corpo.style.color = 'white'
+botao.style.background = 'white'
+span.style.color = 'rgb(124, 124, 199)'
